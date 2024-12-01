be2c8bde4372add4dd3873e170fdd8a666d8c25c
Evg1971 <mosoloeve@gmail.com>
2024-12-01 13:28:54 +0300

    ДЗ: Переводчик чисел в текст

---
+++ Логические условия/dz_2Логические условия.py
@@ -0,0 +1,13 @@
+num = int(input("Введите число от 1 до 5: "))
+if num == 1:
+    print("Соответствующее слово: One")
+elif num == 2:
+    print("Соответствующее слово: Two")
+elif num == 3:
+    print("Соответствующее слово: Three")
+elif num == 4:
+    print("Соответствующее слово: Four")
+elif num == 5:
+    print("Соответствующее слово: Five")
+else:
+    print("Надо ввести число от 1 до 5")
