ec79e8174a7c99958e6120c634427bcee6165b65
Evg1971 <mosoloeve@gmail.com>
2024-12-02 17:36:33 +0300

    ДЗ: Проверка пароля

--- Циклы/dz_2_Циклы.py
+++ Циклы/dz_2_Циклы.py
@@ -1,9 +1,10 @@
 user_password = "123python"
+is_authenticated = False
 
-while True:
+while not is_authenticated:
     input_password = input("Введите пароль: ")
     if input_password == user_password:
         print("Поздравляю! Вы ввели правильный пароль")
-        break
+        is_authenticated = True
     else:
         print("Пароль неверный. Попробуйте еще раз")
