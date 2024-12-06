dfacd7d9aca1055be96bd44992cdde68eb303e88
Evg1971 <mosoloeve@gmail.com>
2024-12-06 11:57:04 +0300

    ДЗ: Применение условий

--- Циклы/dz_4Циклы.py
+++ Циклы/dz_4Циклы.py
@@ -1,11 +1,5 @@
 def summa_numbers():
-    total = 0
-    for x in range(0, 101, 2):
-        total += x
-    return total
-
-
-squares_list = [y ** 2 for y in range(1, 10, 2)]
+    return sum(x for x in range(0, 101, 2))
 
 
 def count_numbers():
@@ -21,6 +15,9 @@ def count_numbers():
         except ValueError:
             print("Ошибка! Введено не число")
     print(f"Количество введенных чисел = {count}")
+
+
+squares_list = [y ** 2 for y in range(1, 10, 2)]
 
 
 print(summa_numbers())
