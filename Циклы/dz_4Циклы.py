484603d38e3cdfdc1d95e9a1a39d28bc12acb2cb
Evg1971 <mosoloeve@gmail.com>
2024-12-06 10:43:46 +0300

    ДЗ: Применение условий

--- Циклы/dz_4Циклы.py
+++ Циклы/dz_4Циклы.py
@@ -9,7 +9,7 @@ squares_list = [y ** 2 for y in range(1, 10, 2)]
 
 
 def count_numbers():
-    count = 0
+    count = 1
     continue_input = True
     while continue_input:
         try:
@@ -20,8 +20,6 @@ def count_numbers():
                 count += 1
         except ValueError:
             print("Ошибка! Введено не число")
-        finally:
-            print(f"Текущее количество введенных чисел:{count}")
     print(f"Количество введенных чисел = {count}")
 
 
