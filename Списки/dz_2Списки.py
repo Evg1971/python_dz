46b05862e01dcbe47267df7ea1400a6e5605ab81
Evg1971 <mosoloeve@gmail.com>
2024-12-07 12:12:52 +0300

    ДЗ: Сложение элементов списков

--- Списки/dz_2Списки.py
+++ Списки/dz_2Списки.py
@@ -1,11 +1,30 @@
-numbers_1 = [1, 2, 3, 4, 5]
-numbers_2 = [6, 7, 8, 9, 10]
+def input_numbers(name):
+    numbers = []
+    print(f"Введите числа для списка {name} (введите 'стоп' для окончания): ")
+    Flag = 1
+    while Flag:
+        user_input = input()
+        if user_input.lower() == 'стоп':
+            break
+        try:
+            num = float(user_input)
+            numbers.append(num)
+        except ValueError:
+            print("Введите корректное число или 'стоп' для окончания")
+    return numbers
+
+roster1 = input_numbers("1")
+roster2 = input_numbers("2")
+
+result_roster = []
+max_length = max(len(roster1), len(roster2))
+
+for i in range(max_length):
+    value1 = roster1[i] if i < len(roster1) else 0
+    value2 = roster2[i] if i < len(roster2) else 0
+    result_roster.append(value1 + value2)
 
 
-if len(numbers_1) != len(numbers_2):
-    print("Списки должны быть одной длины")
-else:
-    numbers_summa: list[int] = []
-    for i in range(len(numbers_1)):
-        numbers_summa.append(numbers_1[i] + numbers_2[i])
-print(numbers_summa)
+print(f"Список № 1: {roster1}")
+print(f"Список № 2: {roster2}")
+print("Новый список: {}".format(result_roster))
